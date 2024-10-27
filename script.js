b3af4631800c0243e1c5de3d1169cf420d4994df
Kdroidwin <144515909+Kdroidwin@users.noreply.github.com>
2024-10-27 21:04:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-// JavaScriptコード
 const toggleButton = document.getElementById('toggle-sidebar');
 const sidebar = document.getElementById('sidebar');
 const container = document.getElementById('container');
 
 // ハンバーガーメニューをクリックしたときの処理
 toggleButton.addEventListener('click', () => {
-    sidebar.classList.toggle('hidden'); // サイドバーの表示/非表示を切り替え
-});
-
-// サイドバー以外の部分をクリックしたときの処理
-container.addEventListener('click', (event) => {
-    if (sidebar.classList.contains('hidden') === false && !sidebar.contains(event.target) && event.target !== toggleButton) {
-        sidebar.classList.add('hidden'); // サイドバーを閉じる
-    }
-});
+    sidebar.classList.toggle('active'); // サ
